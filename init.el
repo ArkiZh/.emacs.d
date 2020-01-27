a1f237dd49fe5b3ce1e4dfb4fd00643d05f87b6b
ArkiZh <1797466468@qq.com>
2020-01-27 16:23:26 +0800

    Doing.

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
 
 ;; 更改显示字体大小 16pt
 ;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :height 109)
+(set-face-attribute 'default nil :height 125)
 
 ;; 关闭启动帮助画面
 (setq inhibit-splash-screen 1)
