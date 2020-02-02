ae05a1f2c0734072d4675776a3ff74d8631fb0b2
ArkiZh <1797466468@qq.com>
2020-02-02 12:01:29 +0800

    Modify.

--- lisp/init-key-bindings.el
+++ lisp/init-key-bindings.el
@@ -3,10 +3,8 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
-
 ;; 这个快捷键绑定可以用之后的插件 counsel 代替
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
-
 
 ;; 配置counsel
 (global-set-key "\C-s" 'swiper)
@@ -14,6 +12,10 @@
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-c p f") 'counsel-git) ;;查找当前所在git仓库管理的文件
+
+;;配置org-mode中的快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
 
 ;; 将自定义函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f2>") 'open-init-file)
