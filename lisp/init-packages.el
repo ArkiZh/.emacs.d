18df4d40a2469f4bbe56a8736da202fa410adc20
ArkiZh <1797466468@qq.com>
2020-07-21 13:29:14 +0800

    Modify python-mode hook's actions: Check pyls and codna home, if not valid, prompt to select one.

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -127,31 +127,29 @@
 
 ;; 配置lsp-mode
 (add-hook 'python-mode-hook (lambda ()
-			      (interactive)
 			      (setq lsp-keymap-prefix "C-c l")  ;;Should be set before require
 			      (require 'lsp-mode)
 			      ;; Install pyls by: pip install 'python-language-server[all]' -i https://pypi.tuna.tsinghua.edu.cn/simple/
 			      ;; Specify you pyls command path, before or after require
-			      (message "=================lsp========")
-			      (if (or (not (boundp 'lsp-pyls-server-command)) (not (file-exists-p lsp-pyls-server-command)))
-				(message "Select python language server: Windows:C:/DevSoft/anaconda/anaconda3_5.3.0/Scripts/pyls Mac:~/dev_tool/miniconda3/envs/ml/bin/pyls")
-				(customize-save-variable 'lsp-pyls-server-command (read-file-name "Select your plys:"))
-				(message "================end lsp")
-				) 
+			      (if (or (not (boundp 'lsp-pyls-server-command)) (not (file-exists-p (car lsp-pyls-server-command))))
+				  (progn 
+				    (message "Select python language server: Windows:C:/DevSoft/anaconda/anaconda3_5.3.0/Scripts/pyls Mac:~/dev_tool/miniconda3/envs/ml/bin/pyls")
+				    (customize-save-variable 'lsp-pyls-server-command (list (read-file-name "Select your plys:")))
+				    )
+				)
 			      ))
 (add-hook 'python-mode-hook #'lsp-deferred)
 ;; 配置realgud，python的debug插件
 (add-hook 'python-mode-hook (lambda () (load-library "realgud")))
 ;; 配置conda环境
 (add-hook 'python-mode-hook (lambda ()
-			      (interactive)
-			      (message "================conda")
 			      (if (or (not (boundp 'codna-anaconda-home)) (not (file-exists-p conda-anaconda-home)))
-				(message "Select conda home: Windows:C:/DevSoft/anaconda/anaconda3_5.3.0 Mac:~/dev_tool/miniconda3")
-				(customize-save-variable 'conda-anaconda-home (read-file-name "Select your conda home:"))
-				(message "================conda end")
-				) ;;Should be set before require, if not the default(~/.anaconda3)
-			      ;; (custom-set-variables '(conda-anaconda-home "/Users/Arki/dev_tool/miniconda3"))  
+				  ;; A valid conda-anacodna-home Should be set before require, if not the default(~/.anaconda3)
+				  (progn
+				    (message "Select conda home: Windows:C:/DevSoft/anaconda/anaconda3_5.3.0 Mac:~/dev_tool/miniconda3")
+				    (customize-save-variable 'conda-anaconda-home (read-file-name "Select your conda home:"))
+				    )
+				)
 			      (require 'conda)
 			      (conda-env-initialize-interactive-shells)  ;;interactive shell support
 			      (conda-env-initialize-eshell)  ;;eshell support
