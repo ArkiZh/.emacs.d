92cb179e374f22811d13c37a6069eea0383f7a25
ArkiZh <1797466468@qq.com>
2021-04-13 19:38:28 +0800

    Config fonts.

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -14,27 +14,12 @@
   "Font config, only for read."
   :group 'arki/config)
 
-(defcustom arki/font-english nil
-  "The font name of English."
-  :group 'arki/font)
-
-(defcustom arki/font-chinese nil
-  "The font name for Chinese."
-  :group 'arki/font)
-
-(defcustom arki/font-size-pair nil
-  "Default font size pair for (english . chinese)"
-  :group 'arki/font)
-
-(defcustom arki/font-size-pair-list nil
-  "This list is used to store matching (englis . chinese) font-size."
-  :group 'arki/font)
 
 (defcustom arki/font-english "DejaVu Sans Mono"
   "English font"
   :group 'arki/font)
 
-(defcustom arki/font-english-size 20
+(defcustom arki/font-english-size 16
   "English font size"
   :group 'arki/font)
 
@@ -46,7 +31,7 @@
   "Chinese font size"
   :group 'arki/font)
 
-(defcustom arki/font-chinese-extra "Source Han Serif CN"
+(defcustom arki/font-chinese-extra "ZhongHuaSongPlane02"
   "Chinese extra font"
   :group 'arki/font)
 
@@ -118,93 +103,59 @@
   )
 
 
-(defun arki/font-step (step-size)
+(defun arki/font-step (step-size font-type)
+  "Set font size by step.
+
+font-type 1 for English font. 2 Chinese. 3 Chinese-extra. 4 symbol"
+  (cond
+   ((equal font-type 1) (arki/set-font-english arki/font-english (+ arki/font-english-size step-size)))
+   ((equal font-type 2) (arki/set-font-chinese arki/font-chinese (+ arki/font-chinese-size step-size)))
+   ((equal font-type 3) (arki/set-font-chinese-extra arki/font-chinese-extra (+ arki/font-chinese-extra-size step-size)))
+   ((equal font-type 4) (arki/set-font-symbol arki/font-symbol (+ arki/font-symbol-size step-size)))
+   (t (message "Unsupported font-type: %s" font-type))
+   )
   )
 
+(defun arki/font-size-adjust ()
+  (interactive)
+  ;; (unless (zerop step)
+  (message "Adjust font size: 1(ENG +) 2(汉 +) 3(生僻 +) 4(Symbol +). Add SHIFT to decrease. ")
+  (set-transient-map
+   (let ((map (make-sparse-keymap)))
+     (define-key map (vector (list ?1))
+       (lambda () (interactive) (arki/font-step arki/font-size-step 1) (arki/font-size-adjust)))
+     (define-key map (vector (list ?!))
+       (lambda () (interactive) (arki/font-step (- arki/font-size-step) 1) (arki/font-size-adjust)))
 
-(defun set-font (font-english font-chinese size-pair)
-  "Setup emacs English and Chinese font on x window-system."
-  (if (font-exist-p font-english)
-      ;; set-frame-font: When called from Lisp, FONT should be a font
-      ;; name (a string), a font object, font entity, or font spec.
-      (set-frame-font (format "%s:pixelsize=%d" font-english (car size-pair)) t)
-    (warn "Font for English doesn't exist, please install it: %s" font-english))
+     (define-key map (vector (list ?2))
+       (lambda () (interactive) (arki/font-step arki/font-size-step 2) (arki/font-size-adjust)))
+     (define-key map (vector (list ?@))
+       (lambda () (interactive) (arki/font-step (- arki/font-size-step) 2) (arki/font-size-adjust)))
 
-  (if (font-exist-p font-chinese)
-      (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        (set-fontset-font (frame-parameter nil 'font) charset
-                          (font-spec :family font-chinese :size (cdr size-pair))))
-    (warn "Font for Chinese doesn't exist, please install it: %s" font-chinese))
+
+     (define-key map (vector (list ?3))
+       (lambda () (interactive) (arki/font-step arki/font-size-step 3) (arki/font-size-adjust)))
+     (define-key map (vector (list ?#))
+       (lambda () (interactive) (arki/font-step (- arki/font-size-step) 3) (arki/font-size-adjust)))
+
+     (define-key map (vector (list ?4))
+       (lambda () (interactive) (arki/font-step arki/font-size-step 4) (arki/font-size-adjust)))
+     (define-key map (vector (list ?$))
+       (lambda () (interactive) (arki/font-step (- arki/font-size-step) 4) (arki/font-size-adjust)))
+     
+     map))
   )
+(arki/define-key "C-c u f" 'arki/font-size-adjust)
 
 
-;; (defun arki/font-step-chinese (step)
-;;   "Increase or decrease Chinese font"
-;;   (let ((new-size (+ step arki/font-chinese-size)))
-;;     (if (not (font-exist-p arki/font-chinese))
-;; 	(warn "Font for Chinese doesn't exist, please install it: %s" arki/font-chinese)
-;;       (dolist (charset '(kana han symbol cjk-misc bopomofo))
-;;         (set-fontset-font (frame-parameter nil 'font) charset
-;;                           (font-spec :family arki/font-chinese :size new-size)))
-;;       (setq arki/font-chinese-size new-size)
-;;       ))
-;;   )
 
-;; 测试一下
-;; abcdef
-;; (arki/font-step-chinese 0.5)
-
-(defun emacs-step-font-size (step)
-  "Increase/Decrease emacs's font size."
-  (let ((scale-steps arki/font-size-pair-list))
-    (if (< step 0) (setq scale-steps (reverse scale-steps)))
-    (setq arki/font-size-pair
-          (or (cadr (member arki/font-size-pair scale-steps))
-              arki/font-size-pair))
-    (when arki/font-size-pair
-      (message "emacs font size set to %.1f" (car arki/font-size-pair))
-      (set-font arki/font-english arki/font-chinese arki/font-size-pair))))
-
-(defun increase-emacs-font-size ()
-  "Decrease emacs's font-size acording arki/font-size-pair-list."
-  (interactive) (emacs-step-font-size 1))
-
-(defun decrease-emacs-font-size ()
-  "Increase emacs's font-size acording arki/font-size-pair-list."
-  (interactive) (emacs-step-font-size -1))
-
-(setq list-faces-sample-text
-      (concat
-       "ABCDEFTHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz\n"
-       "11223344556677889900       壹貳參肆伍陸柒捌玖零"))
-
-;; Only useful when has window server
-(when window-system
-  ;; setup change size font, base on arki/font-size-pair-list
-  (arki/define-key "C-M-=" 'increase-emacs-font-size)
-  (arki/define-key "C-M--" 'decrease-emacs-font-size)
-
-  ;; setup default  English font and Chinese font
-  (setq arki/font-english "DejaVu Sans Mono") ;Source Code Pro
-  (setq arki/font-chinese "Source Han Serif CN")
-  (setq arki/font-size-pair '(16 . 20))
-  (setq arki/font-size-pair-list '(( 5 .  6)
-				   (10 . 12)
-                                   (13 . 16)
-				   (15 . 18)
-				   (16 . 20)
-				   (17 . 20)
-                                   (19 . 22)
-				   (20 . 24)
-				   (21 . 26)
-                                   (24 . 28)
-				   (26 . 32)
-				   (28 . 34)
-                                   (30 . 36)))
-  ;; Setup font size based on arki/font-size-pair
-  (set-font arki/font-english arki/font-chinese arki/font-size-pair)
-  (set-font arki/font-english arki/font-chinese '(15 . 13))
-  )
+;; +----------------------------------------------------+
+;; | [*9.0-18*] [ 20-24 ] [ 26-28 ] [ -30- ] [ -32- ]   |
+;; | 中英文等宽对齐设置：按加号或减号按钮直至此表格对齐 |
+;; | abcdefjhijklmnoprqstuvwxwyABCDEFJHIJkLMNOPQRSTUVXW |
+;; | 𠄀𠄁𠄂𠄃𠄄𠄅𠄆𠄇𠄈𠄉𠄀𠄁𠄂𠄃𠄄𠄅𠄆𠄇𠄈𠄄𠄅𠄆𠄇𠄇𠄆 |
+;; | 英文字号   中文对齐设置    EXT-B 对齐设置    测试  |
+;; +----------------------------------------------------+
 
 ;; 有空时候看看这个用于org-mode 表格对齐的：https://github.com/chen-chao/zh-align.el
 
