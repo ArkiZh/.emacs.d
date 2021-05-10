2fdfcc9f5531b37dd3d2b3d3195cc76f0a1cebb0
ArkiZh <1797466468@qq.com>
2021-05-11 04:34:48 +0800

    Magit config: push current branch to all remotes.

--- lisp/init-project-tools.el
+++ lisp/init-project-tools.el
@@ -1,7 +1,29 @@
 ;; --------------------------------------------------PROJECT UTILS--------------------------------------------------
 (when (require-pack 'magit)
   ;; 配置magit
-  (arki/define-key "C-c u g" 'magit-status))
+  (arki/define-key "C-c u g" 'magit-status)
+  (defun arki/push-current-branch-to-all-remotes ()
+    (interactive)
+    (let* ((cur-branch (magit-get-current-branch))
+	   (remote-names (magit-list-remote-branch-names))
+	   (success-push '())
+	   (failed-push '()))
+      (dolist (remote-name remote-names)
+	(condition-case err (progn
+			      (message "Pushing %S to %S" cur-branch remote-name)
+			      (magit-git-push cur-branch remote-name nil)
+			      (setq success-push (append success-push '(remote-name)))
+			      )
+	  (error
+	   (message "Failed to push %S to %S, due to ERROR: %S" cur-branch remote-name err)
+	   (setq failed-push (append failed-push '(remote-name))))
+	  ))
+      (message "Push %s finished. Success: %s Failed: %s" cur-branch success-push failed-push)
+      t
+      ))
+  (arki/define-key "H" 'arki/push-current-branch-to-all-remotes 'magit-status-mode-map)
+  )
+
 
 ;; https://github.com/Alexander-Miller/treemacs
 (when (require-pack 'treemacs)
