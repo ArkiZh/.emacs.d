1157a24e098e9cdda8e1a3b47f5d9330aa426751
ArkiZh <1797466468@qq.com>
2022-08-17 20:13:49 +0800

    Fix.

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 (defun arki/org-babel-load-file (file &optional compile)
   "Similar to `org-babel-load-file', but changes some logic.  Load Emacs
-  Lisp source code blocks in the Org FILE.  This function exports the
-  source code using `org-babel-tangle' and then loads the resulting file
-  using `load-file'.  With optional prefix argument COMPILE, the tangled
-  Emacs Lisp file is byte-compiled before it is loaded."
+Lisp source code blocks in the Org FILE.  This function exports the
+source code using `org-babel-tangle' and then loads the resulting file
+using `load-file'.  With optional prefix argument COMPILE, the tangled
+Emacs Lisp file is byte-compiled before it is loaded."
   (interactive "fFile to load: \nP")
   (let* ((tangled-file (concat (file-name-sans-extension file) ".el"))
          (tangled-compiled-file (concat (file-name-sans-extension file) ".elc")))
-    (require 'org)    
+    (require 'org)
     ;; Tangle only if the Org file is newer than the Elisp file.
     (when (file-newer-than-file-p file tangled-file)
       (org-babel-tangle-file file tangled-file "emacs-lisp")
@@ -22,6 +22,7 @@
       (load-file tangled-file)
       (message "Loaded: %s" tangled-file))))
 
-
+;; Set arki/package-install-disabled to t, if don't want to install new packages.
+(defvar arki/package-install-disabled nil "Disable package install if t.")
 
 (arki/org-babel-load-file (expand-file-name "readme.org" user-emacs-directory) t)
